8451e173bb511de529b446feae9e6c2b16714186
Vineeth P.S <68630939+KingArthur526@users.noreply.github.com>
2025-11-21 20:31:41 +0530

    Update chunking.py

--- chunking.py
+++ chunking.py
@@ -2,8 +2,6 @@ import pandas as pd
 
 csv_source = " Path of the kaggle dataset"
 
-csv_destination = "Path of the processed csv file"
-
 df = pd.read_csv(csv_source)
 
 sorted_df = df.sort_values(by = "Date", ascending= False)
@@ -11,4 +9,5 @@ sorted_df = df.sort_values(by = "Date", ascending= False)
 chunked_df = sorted_df.sample(n= 300)
 
 chunked_df.to_csv("clean_data.csv", index= False)
+
 
