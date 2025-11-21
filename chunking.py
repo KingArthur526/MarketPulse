b0ea20646546aded069cc937ec967f887d585248
Vineeth P.S <68630939+KingArthur526@users.noreply.github.com>
2025-11-21 20:30:52 +0530

    Update chunking.py
    converted the clean dataframe to a csv file

--- chunking.py
+++ chunking.py
@@ -9,3 +9,6 @@ df = pd.read_csv(csv_source)
 sorted_df = df.sort_values(by = "Date", ascending= False)
 
 chunked_df = sorted_df.sample(n= 300)
+
+chunked_df.to_csv("clean_data.csv", index= False)
+
